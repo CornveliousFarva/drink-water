cdfa177abce38f3b6c907e4177bef069a914cb73
Greg Schuman <gcschuman@hotmail.com>
2021-01-17 18:31:25 -0800

    Added the function to update the big cup.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // Variable declarations
-const waterLeft = document.getElementById('water-left')
 const percentage = document.getElementById('percentage')
 const smallCups = document.querySelectorAll('.cup-small')
 const waterLeft = document.getElementById('water-left')
+const liters = document.getElementById('liters')
+
+// Update big cups declaration 1
+updateBigCup()
 
 //Loop through the small cups
 smallCups.forEach((cup, idx) => {
@@ -23,4 +26,32 @@ function highlightCups(idx){
             cup.classList.remove('full')
         }
     })
+
+    // Update big cups declaration within the highlightCups function
+    updateBigCup()
+}
+
+// Function to update the big cups
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if(fullCups === 0){
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else{
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`   
+    }
+
+    if(fullCups === totalCups){
+        waterLeft.style.visibility = 'hidden'
+        waterLeft.style.height = 0
+    } else{
+        waterLeft.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)}Liter`
+    }
+
+    
 }
